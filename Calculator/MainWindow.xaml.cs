8afd4c61f8dbdca67d132efaecc73cb0aa9f1acc
José Rogério de Almeida Júnior <Jose.Junior@andritz.com>
2022-05-02 17:34:50 -0300

    Add number button handler

--- Calculator/MainWindow.xaml.cs
+++ Calculator/MainWindow.xaml.cs
@@ -1,17 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Calculator
 {
@@ -20,9 +8,30 @@ namespace Calculator
     /// </summary>
     public partial class MainWindow : Window
     {
+        private const string DefaultDisplayResult = "0";
+        
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void NumberButton_Click(object sender, RoutedEventArgs e)
+        {
+            var button = (Button)sender;
+            var buttonText = button.Content.ToString();
+            if (ResultLabel.Content.ToString() == DefaultDisplayResult)
+            {
+                ResultLabel.Content = buttonText;
+            }
+            else
+            {
+                ResultLabel.Content += buttonText;
+            }
+        }
+
+        private void ClearButton_Click(object sender, RoutedEventArgs e)
+        {
+            ResultLabel.Content = DefaultDisplayResult;
         }
     }
 }
